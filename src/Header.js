61787399ac5dd853d44c09daec35667ab2df6f74
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-07-22 01:32:44 +0500

    starts codes linkedin-clone project

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <LinkedInIcon className="linkedin" />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title='me' />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sidebar.scss";
+
+function Sidebar() {
+    return <div className="sidebar">
+      
+  </div>;
+}
+
+export default Sidebar;
